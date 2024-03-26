46a43a449079fd926e4c564ba7bebae2e592f8fe
ERieu <evan.rieu@gmail.com>
2024-03-26 17:18:07 +0100

    update tp Kotlin

--- classe/src/Main.kt
+++ classe/src/Main.kt
@@ -6,7 +6,6 @@ class Compte {
     constructor(balanceInitial: Double) {
         this.balance = balanceInitial
     }
-
     fun getBalance(): Double = this.balance
 
     fun deposer(montant: Double) {
@@ -20,17 +19,22 @@ class Compte {
     }
 }
 
+class Employe(var Matricule: Int, var Nom: String, var Prenom: String, var anneeEmbauche: Int, var Salaire: Double) {
+    var anciennete = 2024
+
+    fun anciennete(annee:Int) {
+        var anneeAnciennete = annee - anneeEmbauche
+    }
+}
+
 fun main() {
+    /*
     val compte = Compte(1000.0)
 
-    println("Balance initial: ${compte.getBalance()}")
     compte.deposer(500.0)
-
-    println("Balance après dépôt: ${compte.getBalance()}")
     compte.retirer(200.0)
-
-    println("Balance après retrait: ${compte.getBalance()}")
     compte.ajoutInteret(0.05)
 
-    println("Balance après ajout intérêt: ${compte.getBalance()}")
+    println("${compte.getBalance()}")
+    */
 }
